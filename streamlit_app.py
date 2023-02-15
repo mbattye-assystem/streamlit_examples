98de13c2b16c82d799730c3eab590f7a769f27a9
Mike Battye <113534168+mbattye-assystem@users.noreply.github.com>
2023-02-15 12:11:24 +0000

    Example App & Requirements

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+with st.form("Main Form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
+
+with st.sidebar:
+    st.radio('Select one:', [1, 2])
